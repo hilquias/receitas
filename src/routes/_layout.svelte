41dd06cdcfdf68fd201754159014775e18330e45
hilquias <>
2020-10-06 00:22:24 -0300

    design minimalista baseado em new.css

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -19,17 +19,6 @@
     export let sections;
 </script>
 
-<style>
-    main {
-        position: relative;
-        max-width: 56em;
-        background-color: white;
-        padding: 2em;
-        margin: 0 auto;
-        box-sizing: border-box;
-    }
-</style>
-
 <Nav segment="{segment}" sections="{sections}" />
 
 <main>
